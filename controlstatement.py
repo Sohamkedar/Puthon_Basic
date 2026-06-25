97d1ca3985c052e1d2fe0b479d6b8069186d9299
Soham kedar <kedarsoham@gmail.com>
2026-06-25 15:39:11 +0530

    update

---
+++ controlstatement.py
@@ -0,0 +1,48 @@
+#Control statements in python
+#Control statements are used to control the flow of execution in a program. 
+#They allow you to make decisions, repeat actions, and handle exceptions. 
+
+#following are the types of control statements in python
+#Decision making its used to control the flow of program
+#if statement,if else statement,if elif statement,nested statement
+
+#looping statements its used to repeat a block of code multiple times
+#while loop,for loop
+
+#Decision making statements
+#1. if statement
+#The if statement is used to execute a block of code only if a specified condition is true. 
+#If the condition is false, the code block is skipped.
+print("if statement")
+a=10
+b=20
+if a>b:
+    print("a is greater")
+
+print("B is greater")
+print("\n\n")
+
+#following are 7 examples of if statement
+#padding work
+
+#2. if else statement
+#The if else statement is used to execute one block of code if a specified condition is true
+print("if else statement")
+a=10
+b=20
+if a>b:
+    print("a is greater")
+else:
+    print("b is greater")
+
+print("\n")
+
+#even odd number example
+c=5
+if (c%2==0):
+    print("c is even")
+else:
+    print("c is odd")
+
+#following are 10 examples of if else statement
+#padding work
---
+++ demo.py
@@ -0,0 +1,2 @@
+print(0 and 10) #and operator returns the first falsy value or the last value if all are truthy
+print(0 or 10) #or operator returns the first truthy value or the last value if all are falsy
